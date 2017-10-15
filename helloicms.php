ae59af3510ef08d6bee3058ad3aee568e6c51824
NeonMy <mazafa@rambler.ru>
2017-10-15 23:40:58 +0300

    first

--- helloicms.php
+++ helloicms.php
@@ -16,12 +16,25 @@ if(!defined('WPINC')){
 	die;
 }
 
-include_once $_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php';
-$wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
+if (!class_exists('wpdb')) {
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/wp-includes/wp-db.php';
+}
+if (!$wpdb) {
+    $wpdb = new wpdb(DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);        
+}
 
 
-register_activation_hook(__FILE__, function($wpdb) {
-    var_dump($wpdb); exit;
+register_activation_hook(__FILE__, function() use($wpdb) {
+    $wpdb->query("CREATE TABLE IF NOT EXISTS `wp_helloicms` ( "
+            . "    `id` int(11) NOT NULL AUTO_INCREMENT, "
+            . "    `name` varchar(255) CHARACTER SET utf8 NOT NULL, "
+            . "    `settings` text CHARACTER SET utf8 NOT NULL, "
+            . "    PRIMARY KEY (`id`) "
+            . "  ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;");
+});
+
+register_deactivation_hook(__FILE__, function() use($wpdb) {
+    $wpdb->query("DROP TABLE wp_helloicms");
 });
 
 //call_user_func($callback);
